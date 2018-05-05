c219d607825eb6cefe9f4d2f9aebcb4e533262dc
CreeperJim <1010660838@qq.com>
2018-05-05 13:55:22 +0800

    change coding

--- devil tower/devil tower/ID.cpp
+++ devil tower/devil tower/ID.cpp
@@ -1,77 +1,77 @@
-/*
-������
-0   FLOOR               �ذ�
-1   WALL                ǽ��
-2   YELLOW DOOR         ����
-3   BLUE DOOR           ����
-4   RED DOOR            ����
-5   MAGIC DOOR          ħ���ţ���Ҫ��������������
-6   STAR                �ǿ�
-7   LAVA                �ҽ�
-8   STAIR UP             ��¥��
-9   STAIR DOWN           ��¥��
-10  MONEY STORE          ��Ǯ�̵�
-11  EXPERIENCE STORE     �����̵�
+﻿/*
+地形类
+0   FLOOR               地板
+1   WALL                墙壁
+2   YELLOW DOOR         黄门
+3   BLUE DOOR           蓝门
+4   RED DOOR            红门
+5   MAGIC DOOR          魔法门（需要特殊条件开启）
+6   STAR                星空
+7   LAVA                岩浆
+8   STAIR UP             上楼梯
+9   STAIR DOWN           下楼梯
+10  MONEY STORE          花钱商店
+11  EXPERIENCE STORE     经验商店
 
 
 
 
-������
-100  HERO1              Ӣ�۳����
-101  HERO2              Ӣ�۱������
-102  HERO3              Ӣ�۳���
-103  HERO4              Ӣ�۳���
-104  ELDER              �׷�����
-105  MERCHANT           ����
-106  PRINCESS           ����
+人物类
+100  HERO1              英雄朝玩家
+101  HERO2              英雄背向玩家
+102  HERO3              英雄朝左
+103  HERO4              英雄朝右
+104  ELDER              白发老人
+105  MERCHANT           商人
+106  PRINCESS           公主
 
 
 
-������
-200  YELLOW KEY          ��Կ��
-201  BLUE KEY            ��Կ��
-202  RED KEY             ��Կ��
+道具类
+200  YELLOW KEY          黄钥匙
+201  BLUE KEY            蓝钥匙
+202  RED KEY             红钥匙
 
-203  RED GEM             �챦ʯ
-204  BLUE GEM            ����ʯ
-205  GREEN GEM           �̱�ʯ
-206  SMALL ELIXER        СѪƿ
-207  MIDIUM ELIXER       ��Ѫƿ
-208  LARGE ELIXER        ��Ѫƿ
+203  RED GEM             红宝石
+204  BLUE GEM            蓝宝石
+205  GREEN GEM           绿宝石
+206  SMALL ELIXER        小血瓶
+207  MIDIUM ELIXER       中血瓶
+208  LARGE ELIXER        大血瓶
 
-209  TRANSFER            �������
-210  DICTIONARY          �ֵ�
-211  BIG COIN            ����
+209  TRANSFER            跳层道具
+210  DICTIONARY          字典
+211  BIG COIN            大金币
 
 
 
-������
-300  GREEN SLIME         ��ɫʷ��ķ
-301  RED SLIME           ��ɫʷ��ķ
-302  BLACK SLIME         ��ɫʷ��ķ
-303  SLIME KING          ʷ��ķ��
+怪物类
+300  GREEN SLIME         绿色史莱姆
+301  RED SLIME           红色史莱姆
+302  BLACK SLIME         黑色史莱姆
+303  SLIME KING          史莱姆王
 
-310  SMALL BAT           С����
-311  BIG BAT             ������
-312  RED BAT             ������
+310  SMALL BAT           小蝙蝠
+311  BIG BAT             大蝙蝠
+312  RED BAT             红蝙蝠
 
-320  SKELETON            ����
-321  ARMED SKELETON      ����ʿ��
-322  CAPTAIN SKELETON    ���öӳ�
+320  SKELETON            骷髅
+321  ARMED SKELETON      骷髅士兵
+322  CAPTAIN SKELETON    骷髅队长
 
-330  BLUE WITCH          ��ɫŮ��
-331  YELLOW WITCH        ��ɫŮ��
-332  RED WITCH           ��ɫŮ��
+330  BLUE WITCH          蓝色女巫
+331  YELLOW WITCH        黄色女巫
+332  RED WITCH           红色女巫
 
-340  YELLOW GUARD        ��ɫ����
-341  BLUE GUARD          ��ɫ����
-342  RED GUARD           ��ɫ����
+340  YELLOW GUARD        黄色守卫
+341  BLUE GUARD          蓝色守卫
+342  RED GUARD           红色守卫
 
-350  ORCISH              ����
-351  ARMED ORCISH        ����ʿ��
+350  ORCISH              兽人
+351  ARMED ORCISH        兽人士兵
 
-360  YELLOW KNIGHT       ����ʿ
-361  RED KNIGHT          ����ʿ
-362  DOUBLE SWORDS KNIGHT ˫�ֽ�ʿ
+360  YELLOW KNIGHT       黄骑士
+361  RED KNIGHT          红骑士
+362  DOUBLE SWORDS KNIGHT 双手剑士
 
 */
